61db73f334f795742bdb584cbde962398b8d6eb9
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2022-03-16 12:36:18 +0100

    feat: update typings

--- public/lib/services/languages/languages.service.types.ts
+++ public/lib/services/languages/languages.service.types.ts
@@ -1,8 +1,20 @@
-import { SiteDetailModel } from '@redactie/sites-module';
 import { EmbeddedResourceResponse } from '@redactie/utils';
 
 export type LanguagesSchema = EmbeddedResourceResponse<'languages', LanguageSchema>;
 
+interface SitesDetailData {
+	name: string;
+	url: string;
+	contentTypes: string[];
+	languages: string[];
+	description: string;
+	modulesConfig: unknown[];
+}
+
+interface SiteDetailModel {
+	data: SitesDetailData;
+	uuid: string;
+}
 export interface LanguageSchema {
 	uuid: string;
 	_id?: string;
