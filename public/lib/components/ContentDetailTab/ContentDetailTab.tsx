536a75491368a8fffbbffaf1b69ef8e00978fab7
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2022-04-13 13:10:58 +0200

    feat: add translation

--- public/lib/components/ContentDetailTab/ContentDetailTab.tsx
+++ public/lib/components/ContentDetailTab/ContentDetailTab.tsx
@@ -1,21 +1,53 @@
 import { Table } from '@acpaas-ui/react-editorial-components';
 import { ExternalTabProps } from '@redactie/content-module';
-import { LoadingState, useNavigate } from '@redactie/utils';
+import { LoadingState, SelectOption, useNavigate } from '@redactie/utils';
 import React, { FC, useEffect, useMemo } from 'react';
 
 import contentConnector from '../../connectors/content';
 import translationsConnector, { CORE_TRANSLATIONS } from '../../connectors/translations';
+import workflowsConnector from '../../connectors/workflows';
 import { useActiveLanguagesForSite } from '../../hooks';
 import { contentCreate, contentDetail, contentEdit, SITES_ROOT } from '../../language.const';
 
 import { TRANSLATIONS_COLUMNS } from './ContentDetailTab.const';
-import { TranslationTableRow } from './ContentDetailTab.types';
+import { ContentSystemNames, TranslationTableRow } from './ContentDetailTab.types';
 
 const ContentDetailTab: FC<ExternalTabProps> = ({ siteId, contentType, contentItem }) => {
 	const [t] = translationsConnector.useCoreTranslation();
-	const { generatePath, navigate } = useNavigate(SITES_ROOT);
+	const { generatePath } = useNavigate(SITES_ROOT);
 	const [, languages] = useActiveLanguagesForSite(siteId);
 	const [contentLoadingState, content] = contentConnector.useContent();
+	const {
+		pagination: statusesPagination,
+	} = workflowsConnector.hooks.usePaginatedWorkflowStatuses(
+		{
+			page: 1,
+			pagesize: -1,
+		},
+		{
+			siteId,
+		}
+	);
+
+	const workflowStatuses = useMemo<Record<string, SelectOption>>(() => {
+		if (!statusesPagination?.data) {
+			return {};
+		}
+
+		return statusesPagination.data.reduce((acc, status) => {
+			if (status.data.systemName === ContentSystemNames.NEW) {
+				return acc;
+			}
+
+			return {
+				...acc,
+				[status.data.systemName as string]: {
+					label: status.data.name,
+					value: status.data.systemName,
+				},
+			};
+		}, {});
+	}, [statusesPagination]);
 
 	useEffect(() => {
 		contentConnector.getContent(siteId, {
@@ -42,7 +74,15 @@ const ContentDetailTab: FC<ExternalTabProps> = ({ siteId, contentType, contentIt
 					language: `${language.name} (${language.key.toUpperCase()})`,
 					title: contentItemLang?.meta?.label || '',
 					lastModified: contentItemLang?.meta?.lastModified || '',
-					status: contentItemLang?.meta?.status || '',
+					status:
+						workflowStatuses[
+							contentItemLang?.meta?.historySummary?.workflowState as string
+						]?.label ||
+						statusesPagination?.data.find(
+							status =>
+								status.data.technicalState ===
+								contentItemLang?.meta?.historySummary?.latestStatus
+						)?.data.name,
 					published: contentItemLang?.meta?.published,
 					viewPath:
 						contentItemLang &&
--- public/lib/components/ContentDetailTab/ContentDetailTab.types.ts
+++ public/lib/components/ContentDetailTab/ContentDetailTab.types.ts
@@ -2,9 +2,13 @@ export interface TranslationTableRow {
 	language: string;
 	title: string;
 	lastModified: string;
-	status: string;
+	status?: string;
 	published?: boolean;
 	viewPath?: string;
 	editPath?: string;
 	createPath?: string;
+}
+
+export enum ContentSystemNames {
+	NEW = 'nieuw',
 }
