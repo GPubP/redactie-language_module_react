cd199ba149ac8588d5aca6838b78d786dd9375cb
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2022-04-28 09:57:29 +0200

    feat: use translation.const.ts

--- public/lib/i18next/translations.const.ts
+++ public/lib/i18next/translations.const.ts
@@ -7,6 +7,10 @@ const tKey = translationsConnector.core.tKey;
 // TODO: Place all your copy here
 const MODULE_TRANSLATIONS = Object.freeze<I18NextTranslations>({
 	LANGUAGES_TITLE: tKey('LANGUAGES_TITLE', 'Talen'),
+	LANGUAGES_DELETE_DESCRIPTION: tKey(
+		'LANGUAGES_DELETE_DESCRIPTION',
+		'Er zijn binnen deze tenant geen sites die deze taal gebruiken. Als je deze taal deactiveert kan deze niet meer geactiveerd worden op een site.'
+	),
 });
 
 export { MODULE_TRANSLATIONS };
